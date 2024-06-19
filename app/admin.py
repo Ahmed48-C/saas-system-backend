3f5bcb5af9678a7120b9b41b68ef8fd05d45af48
Azzam Makki <azzam@smartretail.co>
2024-06-19 22:33:00 +0800

    Create Model for testing and Add to Admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import TestModel
+
+@admin.register(TestModel)
+class TestModelAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'name',
+    ]
