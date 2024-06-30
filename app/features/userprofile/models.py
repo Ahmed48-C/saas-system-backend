48498db25152c1b817e5ed19bd85e00c8905ae63
Azzam Makki <azzam@smartretail.co>
2024-06-30 19:14:04 +0800

    Link userprofile and location with the Operator model

--- app/features/userprofile/models.py
+++ app/features/userprofile/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from app.features.operator.models import Operator
 from app.features.location.models import Location
 from app.features.userprofile.querymanagers import UserProfileQueryManager
 
@@ -7,6 +8,7 @@ from app.features.userprofile.querymanagers import UserProfileQueryManager
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     location = models.ForeignKey(Location, on_delete=models.PROTECT, null=True, blank=True)
+    operator = models.ForeignKey(Operator, on_delete=models.PROTECT, null=True, blank=True)
     objects = UserProfileQueryManager()
 
     def __str__(self):
