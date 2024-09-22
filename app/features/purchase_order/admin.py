dba828d1c93c68a1dc4ba0dc5ed8af781f2af163
Ahmed48-C <85570965+Ahmed48-C@users.noreply.github.com>
2024-09-22 14:49:58 +0800

    added product items in purchase order

--- app/features/purchase_order/admin.py
+++ app/features/purchase_order/admin.py
@@ -1,20 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.features.purchase_order.models import PurchaseOrder
+from app.features.purchase_order.models import PurchaseOrder, PurchaseItem
 
+
+class PurchaseItemAdminInline(admin.TabularInline):
+    model = PurchaseItem
+
+    fields = (
+        'id',
+        'price',
+        'total',
+        'quantity',
+        'product',
+    )
 
 @admin.register(PurchaseOrder)
 class PurchaseOrderAdmin(admin.ModelAdmin):
 
+    inlines = (PurchaseItemAdminInline,)
+
     list_display = [
-        'name',
-        'price',
-        'quantity',
+        'code',
         'total',
         'status',
         'operator',
         'store',
-        'product',
         'balance',
     ]
